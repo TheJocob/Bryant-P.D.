d56ce78c2584977ffaae704c55640aad260a68b4
TheJocob <alex50546@yahoo.com>
2014-12-16 08:45:49 -0600

    BryantP.D.
    An app used to help the BryantP.D.

---
+++ Bryant P.D./ThirdViewController.h
@@ -0,0 +1,15 @@
+//
+//  ThirdViewController.h
+//  Bryant P.D.
+//
+//  Created by ALFREDO RAMIREZ on 11/5/14.
+//  Copyright (c) 2014 ALFREDO RAMIREZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController {
+    IBOutlet UILabel *label;
+}
+
+@end
